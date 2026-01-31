46d7210d19353ff08d28e1ded4979cdf8ec4af1d
Jamil Dellawar <jamild@mit.edu>
2026-01-31 11:12:42 -0500

    Remove hardcoded API keys
    Replace Spotify env variables with placeholders, delete old keys

--- addNewAlbum.py
+++ addNewAlbum.py
@@ -5,9 +5,9 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 import json
 
-DEVICE_ID="684d1697e3deb93b5a07afe11b468e1416a926de"
-CLIENT_ID="72dc9b1300b5426d844363d7cd55a9a4"
-CLIENT_SECRET="714704b00c694161811da07eb4187b98"
+DEVICE_ID="XXX"
+CLIENT_ID="XXX"
+CLIENT_SECRET="XXX"
 
 
 
